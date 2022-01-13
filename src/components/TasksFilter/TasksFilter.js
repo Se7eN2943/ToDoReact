2212b9676ae5349e6dae578d327134ec5dff5130
Se7eN <flip2943@gmail.com>
2022-01-13 22:32:05 +0300

    п

--- src/components/TasksFilter/TasksFilter.js
+++ src/components/TasksFilter/TasksFilter.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+const AppHeader = () => <h1>ToDoes</h1>
+
 const TasksFilter = () => {
-    return <input placeholder="Название дела" />
+    return (
+        <div>
+            <header class="header">
+                <AppHeader />
+                <input class="new-todo" placeholder="What needs to be done?" autofocus />
+            </header>
+        </div>
+    )
 }
 
 export default TasksFilter
+
+
